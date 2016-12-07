0045bdd30dbc3868b8831592eb4741be8dbdbf48
Andrej Vajagic <drejcreative@gmail.com>
2016-12-08 00:18:38 +0100

    removing startup bat

--- start.bat
+++
@@ -1,9 +0,0 @@
-COLOR 0A
-F:
-chdir F:\Work Drej Creative\WORKSPACE\WIP\Test\client
-start gulp
-
-chdir F:\Work Drej Creative\WORKSPACE\WIP\Test\server
-start npm test
-
-open chrome http://localhost:3000/#/
